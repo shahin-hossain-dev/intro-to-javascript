8834db3649eb993baa1410e14ec12f2898f32252
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-26 00:39:42 +0600

    16-7 Introduction To Arithmetic Operators

---
+++ operation.js
@@ -0,0 +1,36 @@
+var eggPrice = 20;
+var onionPrice = 30;
+
+// addition 
+console.log(eggPrice + onionPrice);
+
+// subtraction 
+console.log(eggPrice - onionPrice);
+
+// multiply 
+console.log(eggPrice * onionPrice) // * -> astrict sign for multiply
+
+// divide 
+
+console.log(10 / 2);
+
+// remainder / ভাগশেষ পেতে চাইলে % percent sign use করতে হবে।
+//% Modulus (Division Remainder)
+
+console.log(10 % 3);
+
+
+// variable reassign 
+// reassing করার সময় var key লিখতে হয় না । var key শুধু মাত্র variable declare করার সময় ব্যবহার করা হয়।
+
+var price = 50;
+price = price + 10;
+
+var newPrice = price + 50;
+
+var discountPrice = newPrice - 10;
+
+console.log(discountPrice)
+
+
+
