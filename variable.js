5528770757c24f514a98906d2136e19f9820d7f2
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-25 20:52:26 +0600

    16-3 What Is A Variable, Five Things You Need To Declare A Variable

---
+++ variable.js
@@ -0,0 +1,26 @@
+/**
+ * ***********************
+ *       VARIABLE
+ * ***********************
+ * => কোনো ডাটা store করে রাখে যে জিনিস তাকে বলে Variable.
+ *
+ * => 5 things to declare a variable
+ *
+ * 1. keyword: var, let , const
+ * 2. name: variable এর একটা নাম থাকবে।
+ * 3. assign: (=) একটা সামান চিহ্ন দিয়ে Assign করতে হবে।
+ * 4. Value: যেকোনো একটা মান বা ভ্যালু থাকবে ।
+ * 5. semicolon: (;) শেষ বুঝাতে একটি সেমিকোলন দিতে হবে।
+ */
+// Variable Example
+var weight = 38;
+var price = 50;
+
+// যদি variable এ কোনো মান সেটা না করা হয় তাহলে সেটা undefined হবে। অর্থাৎ কোনো মান define করা হয়নি।
+var age;
+console.log(price)
+
+// variable এর কোনো মান চেন্জ করা যায়। চেন্জ করার সময় var keyword লিখতে হয় না।
+var money = 500;
+money = 5000;
+console.log(money)
