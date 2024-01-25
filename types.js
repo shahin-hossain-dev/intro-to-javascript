aecb7d4e74af3a4180795515c36209aa971b80a1
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-25 21:38:14 +0600

    16-4 Variable Data Types, Numeric, String, Boolean

---
+++ types.js
@@ -0,0 +1,34 @@
+/**
+ * ****************
+ *  DATA TYPES
+ * ****************
+ * 1. Number/Numeric:
+ * 2. String
+ */
+// --------------number type variable-----------------
+// যে variable এর মধ্যে শুধু নাম্বার থাকে সেগুলোকে নাম্বার type variable বলে।
+var age = 25;
+
+// -------------String type variable---------------------
+
+// যদি কোন মানের মধ্যে কোনো “” কোটশন ছাড়া নাম লেখা হয় হয় তাহলে সেটা programming language variable হিসাবে ধরে নেয়।
+// var name = shahin;
+// যদি কোনো string লিখতে হয় তাহলে তাকে Double Quotation ("") এর মধ্যে রাখতে হবে।
+var name = 'shahin';
+var country = 'Bangladesh'
+// console.log(name)
+
+// -----------boolean type variable ----------
+
+// যদি কোন জিনিস সত্য কিংবা মিথ্যা হয় বা yes or no হয় তাহলে boolean type variable দিয়ে define করতে হবে।
+
+var isRich = true;
+console.log(isRich)
+
+// type check করার জন্য ব্যবহার করা হয় "type of".
+
+console.log(typeof isRich)
+console.log(typeof country)
+console.log(typeof age)
+
+
