6f63626d590362299a3ee6a848481f023c5b381b
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-26 19:14:30 +0600

    16-8 (Advanced) Mathematical Operation Shorthand

---
+++ concat.js
@@ -0,0 +1,18 @@
+var first = 'Mobarok';
+var second = 'Tobarok';
+
+var fullName = first + second;
+
+// console.log(fullName) //MobarokTobarok
+
+// ----------------concat---------------------
+
+// যদি যোগ করার সময় ২টা value এর মধ্যে যেকোনো একটা string হয় তাহলে দুটি পাশাপাশি বসে যাবে বা concat হয়ে যাবে ।
+
+console.log('20' + 40); //2040
+
+// কিন্তু বিয়োগ, গুন, ভাগের ক্ষেত্রে সাধারন অপারেশনের মতো অপারেশন হবে।
+
+console.log('40' - 10); //30
+console.log('20' * 2); // 40
+console.log(30 / '2'); //15
---
+++ shorthand.js
@@ -0,0 +1,23 @@
+/**
+ * *******************
+ * SHORT HAND
+ * *******************
+ */
+// ------------Addition----------------->
+var price = 50;
+// price = price - 10; 
+price -= 10; //shorthand of price = price - 10; 
+console.log(price)
+
+var fridge = 35000;
+// fridge = fridge - 1000;
+fridge -= 1000; //shorthand of fridge = fridge - 1000;
+
+console.log(fridge)
+
+// -----------------Multiply---------------------
+
+var orangePrice = 30;
+// orangePrice = orangePrice * 3;
+orangePrice *= 3; //shorthand of orangePrice = orangePrice * 3;
+console.log(orangePrice)
